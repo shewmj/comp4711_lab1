e589178be75c8bfd17c538066c0fe2ea71f2299a
MatthewShew <shewmj@gmail.com>
2018-01-11 14:40:50 -0800

    finished lab

--- index.php
+++ index.php
@@ -10,8 +10,55 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        
         <?php
-            echo 'hello world. I live. muhuahaha';
+        
+            echo 'hehehh';
+        
+            include('Student.php');
+
+            $students = array();
+
+            
+
+            $first = new Student();
+            $first->surname = "Doe";
+            $first->first_name = "John";
+            $first->add_email('home','john@doe.com');
+            $first->add_email('work','jdoe@mcdonalds.com');
+            $first->add_grade(65);
+            $first->add_grade(75);
+            $first->add_grade(55);
+            $students['j123'] = $first;
+            
+            $second = new Student();
+            $second->surname = "Einstein";
+            $second->first_name = "Albert";
+            $second->add_email('home','albert@braniacs.com');
+            $second->add_email('work1','a_einstein@bcit.ca');
+            $second->add_email('work2','albert@physics.mit.edu');
+            $second->add_grade(95);
+            $second->add_grade(80);
+            $second->add_grade(50);
+            $students['a456'] = $second;
+            
+            
+            $third = new Student();
+            $third->surname = "Shew";
+            $third->first_name = "Matthew";
+            $third->add_email('home','Matt@gmail.com');
+            $third->add_email('work1','mjs@bcit.ca');
+            $third->add_email('work2','matthew@physics.mit.edu');
+            $third->add_grade(56);
+            $third->add_grade(45);
+            $third->add_grade(98);
+            $students['M342'] = $third;
+            
+            ksort($students); // one of the many sort functions
+            
+            foreach($students as $student)
+                echo $student->toString();
+            
         ?>
     </body>
 </html>
